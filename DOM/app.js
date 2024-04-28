e3c7d6b305400d2ee23e3b6701b430f054f8af71
i-yamamotopro <yamamoto.ikki.tech@gmail.com>
2024-04-28 13:49:52 +0900

    要素の取得

---
+++ DOM/app.js
@@ -0,0 +1,8 @@
+// ID要素を取得
+const id = document.getElementById('h1heading');
+
+// 要素をCSSセレクター形式で取得
+// 初めに見つかった要素
+const select = document.querySelector('input[type=checkbox]');
+// 要素の全て
+const selectAll = document.querySelectorAll('.done');
