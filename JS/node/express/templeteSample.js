faae74bb9ea8e1b10bb9843a6016f4025bd7a3e9
i-yamamotopro <yamamoto.ikki.tech@gmail.com>
2024-04-30 05:04:35 +0900

    テンプレートエンジン(EJS)設定とテンプレートファイルの読み込み型

---
+++ JS/node/express/templeteSample.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// テンプレートエンジンの宣言(EJS)
+app.set('view engine', 'ejs');
+
+
+// サーバー起動
+app.listen(port, () => {
+    console.log('リクエストをポート3000で待ち受け中...');
+});
+
+
+
+
+// ルートパス
+app.get('/', (req, res) => {
+    // デフォルトで/viewsを見に行くため記述不要
+    // 拡張子ejsをapp.setでセットしているので記述不要
+    res.render('home');
+});
+
+// 想定外のパス
+app.get('*', (req, res) => {
+    res.send('そんなパスは知らない！！！');
+});
+
+
