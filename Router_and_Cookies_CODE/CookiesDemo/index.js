d90653fa9b43d732ca555d18a2f4ec33bdced0e4
i-yamamotopro <yamamoto.ikki.tech@gmail.com>
2024-05-06 04:05:23 +0900

    コメント付与

--- Router_and_Cookies_CODE/CookiesDemo/index.js
+++ Router_and_Cookies_CODE/CookiesDemo/index.js
@@ -2,19 +2,24 @@ const express = require('express');
 const app = express();
 
 const cookieParser = require('cookie-parser');
+// クッキーをリクエストで受け取るためのミドルウェア
 app.use(cookieParser('mysecret'));
 
+// クッキーを受け取る
 app.get('/greet', (req, res) => {
     const { name = 'anonymous' } = req.cookies;
     res.send(`ようこそ ${name} さん`);
 });
 
+// クッキーをセット
 app.get('/setname', (req, res) => {
     res.cookie('name', 'tanakajiro');
     res.cookie('animal', 'cat');
     res.send('クッキー送ったよ！！！');
 });
 
+// 署名付きクッキーをセット
+// 改ざんされるとオブジェクトの存在が消える、または値にfalseが変える
 app.get('/getsignedcookie', (req, res) => {
     res.cookie('fruit', 'grape', { signed: true });
     res.send('署名付きクッキーを返したよ！');
